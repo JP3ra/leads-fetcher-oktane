91c06034512193330d009165bc990a8622d25bb5
Jyotiprakash Panda <92199359+JP3ra@users.noreply.github.com>
2025-06-08 17:39:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.get("/extract-listings", async (req, res) => {
   }
 });
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
