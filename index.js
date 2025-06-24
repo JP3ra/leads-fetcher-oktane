eaff3cfd438e57fa57e58eedd0fe4faadba40051
Om Patil <ompatil2002@gmail.com>
2025-06-24 10:08:01 +0530

    paused apify

--- index.js
+++ index.js
@@ -38,101 +38,101 @@ app.get("/extract-listings", async (req, res) => {
 
 
     // Step 1: Create request payload for bizbuysell
-    const BIZREQUEST_PAYLOAD = {
-      maxItems: 100,
-      startUrls: [
-        BIZSTART_URL
-      ],
-    };
+    // const BIZREQUEST_PAYLOAD = {
+    //   maxItems: 100,
+    //   startUrls: [
+    //     BIZSTART_URL
+    //   ],
+    // };
 
-    // Step 2: Request BizBuySell data
-    console.log("Using Apfiy actor to fetch BizBuySell data...");
-    const bizResp = await axios.post(BIZBUYSELL_ENDPOINT, BIZREQUEST_PAYLOAD);
-    if (bizResp.status !== 201) {
-      return res.status(500).json({ error: `Failed to fetch data from Apify. Status: ${bizResp.status}` });
-    }
-    const rawBizData = bizResp.data;
-    console.log("Data fetched from BizBuySell apify actor successfully.");
+    // // Step 2: Request BizBuySell data
+    // console.log("Using Apfiy actor to fetch BizBuySell data...");
+    // const bizResp = await axios.post(BIZBUYSELL_ENDPOINT, BIZREQUEST_PAYLOAD);
+    // if (bizResp.status !== 201) {
+    //   return res.status(500).json({ error: `Failed to fetch data from Apify. Status: ${bizResp.status}` });
+    // }
+    // const rawBizData = bizResp.data;
+    // console.log("Data fetched from BizBuySell apify actor successfully.");
 
-    // Step 3: Extract & transform BizBuySell data from structured array
-    const extractedData = rawBizData.map((item) => {
-      const [firstName, ...rest] = (item["INTERMEDIARY NAME"] || "").trim().split(" ");
-      const lastName = rest.length > 0 ? rest.join(" ") : null;
+    // // Step 3: Extract & transform BizBuySell data from structured array
+    // const extractedData = rawBizData.map((item) => {
+    //   const [firstName, ...rest] = (item["INTERMEDIARY NAME"] || "").trim().split(" ");
+    //   const lastName = rest.length > 0 ? rest.join(" ") : null;
 
-      return {
-        "DATE ADDED": item["DATE ADDED"],
-        "LISTING EXTRACTED ON": istDate,
-        SOURCE: "BIZ BUY SELL",
-        LOCATION: item["LOCATION"] || null,
-        TITLE: item["TITLE"] || null,
-        PRICE: item["PRICE"] || null,
-        EBITDA: item["EBITDA"] || "Not Disclosed",
-        REVENUE: item["REVENUE"] || null,
-        "CASH FLOW": item["CASH FLOW"] || null,
-        "LINK TO DEAL": item["LINK TO DEAL"] || null,
-        "BROKER FIRM": item["INTERMEDIARY FIRM"] || null,
-        "BROKER NAME": item["INTERMEDIARY NAME"] || null,
-        "BROKER FIRST NAME": firstName || null,
-        "BROKER LAST NAME": lastName,
-        "BROKER PHONE NUMBER": item["INTERMEDIARY PHONE"] || null,
-        "BROKER EMAIL": "N/A"
-      };
-    });
-
-
+    //   return {
+    //     "DATE ADDED": item["DATE ADDED"],
+    //     "LISTING EXTRACTED ON": istDate,
+    //     SOURCE: "BIZ BUY SELL",
+    //     LOCATION: item["LOCATION"] || null,
+    //     TITLE: item["TITLE"] || null,
+    //     PRICE: item["PRICE"] || null,
+    //     EBITDA: item["EBITDA"] || "Not Disclosed",
+    //     REVENUE: item["REVENUE"] || null,
+    //     "CASH FLOW": item["CASH FLOW"] || null,
+    //     "LINK TO DEAL": item["LINK TO DEAL"] || null,
+    //     "BROKER FIRM": item["INTERMEDIARY FIRM"] || null,
+    //     "BROKER NAME": item["INTERMEDIARY NAME"] || null,
+    //     "BROKER FIRST NAME": firstName || null,
+    //     "BROKER LAST NAME": lastName,
+    //     "BROKER PHONE NUMBER": item["INTERMEDIARY PHONE"] || null,
+    //     "BROKER EMAIL": "N/A"
+    //   };
+    // });
 
 
-    // Step 4: Extract zillow starting urls
-
-    console.log("Fetchiong Zillow listing URLs...");
-    const zillowRespo = await retriveListingUrl();
-    if (!zillowRespo) {
-      console.error(`Failed to fetch data from Apify for Zillow normal. Status: ${zillowRespo?.status}`);
-      return;
-    }
-    const listingUrls = zillowRespo;
-    if (!listingUrls) {
-      console.error("Listing URLs not found in response.");
-      return;
-    }
 
 
-    // Step 5: Retrieve information from each listing
-    console.log("Starting Zillow extraction...");
-    const detailZillowRespo = await scrapeZillowListings(listingUrls);
-    if (!detailZillowRespo) {
-      console.error(`Failed to fetch detailed data from Apify for Zillow Detail. Status: ${detailZillowRespo?.status}`);
-      return;
-    }
-    const rawZillowData = detailZillowRespo;
+    // // Step 4: Extract zillow starting urls
 
-    console.log("Zillow extraction completed successfully.");
+    // console.log("Fetchiong Zillow listing URLs...");
+    // const zillowRespo = await retriveListingUrl();
+    // if (!zillowRespo) {
+    //   console.error(`Failed to fetch data from Apify for Zillow normal. Status: ${zillowRespo?.status}`);
+    //   return;
+    // }
+    // const listingUrls = zillowRespo;
+    // if (!listingUrls) {
+    //   console.error("Listing URLs not found in response.");
+    //   return;
+    // }
 
-    // Step 7: Extract & transform Zillow data from structured array
-    rawZillowData.forEach((item) => {
-      const fullName = item.attributionInfo?.agentName || "";
-      const [firstName, ...rest] = fullName.trim().split(" ");
-      const lastName = rest.length > 0 ? rest.join(" ") : null;
 
-      extractedData.push({
-        "DATE ADDED": findDate(item.daysOnZillow),
-        "LISTING EXTRACTED ON": istDate,
-        SOURCE: "ZILLOW",
-        LOCATION: (item.city && item.state) ? `${item.city}, ${item.state}` : "N/A",
-        TITLE: "N/A",
-        PRICE: item.price || "N/A",
-        EBITDA: "Not Disclosed",
-        REVENUE: "Not Disclosed",
-        "CASH FLOW": "Not Disclosed",
-        "LINK TO DEAL": `https://www.zillow.com/${item.hdpUrl?.replace(/^\/?/, '')}`,
-        "BROKER FIRM": item.attributionInfo?.brokerName || "N/A",
-        "BROKER NAME": fullName || "N/A",
-        "BROKER FIRST NAME": firstName || "N/A",
-        "BROKER LAST NAME": lastName || "N/A",
-        "BROKER PHONE NUMBER": item.attributionInfo?.agentPhoneNumber || "N/A",
-        "BROKER EMAIL": "N/A"
-      });
-    });
+    // // Step 5: Retrieve information from each listing
+    // console.log("Starting Zillow extraction...");
+    // const detailZillowRespo = await scrapeZillowListings(listingUrls);
+    // if (!detailZillowRespo) {
+    //   console.error(`Failed to fetch detailed data from Apify for Zillow Detail. Status: ${detailZillowRespo?.status}`);
+    //   return;
+    // }
+    // const rawZillowData = detailZillowRespo;
+
+    // console.log("Zillow extraction completed successfully.");
+
+    // // Step 7: Extract & transform Zillow data from structured array
+    // rawZillowData.forEach((item) => {
+    //   const fullName = item.attributionInfo?.agentName || "";
+    //   const [firstName, ...rest] = fullName.trim().split(" ");
+    //   const lastName = rest.length > 0 ? rest.join(" ") : null;
+
+    //   extractedData.push({
+    //     "DATE ADDED": findDate(item.daysOnZillow),
+    //     "LISTING EXTRACTED ON": istDate,
+    //     SOURCE: "ZILLOW",
+    //     LOCATION: (item.city && item.state) ? `${item.city}, ${item.state}` : "N/A",
+    //     TITLE: "N/A",
+    //     PRICE: item.price || "N/A",
+    //     EBITDA: "Not Disclosed",
+    //     REVENUE: "Not Disclosed",
+    //     "CASH FLOW": "Not Disclosed",
+    //     "LINK TO DEAL": `https://www.zillow.com/${item.hdpUrl?.replace(/^\/?/, '')}`,
+    //     "BROKER FIRM": item.attributionInfo?.brokerName || "N/A",
+    //     "BROKER NAME": fullName || "N/A",
+    //     "BROKER FIRST NAME": firstName || "N/A",
+    //     "BROKER LAST NAME": lastName || "N/A",
+    //     "BROKER PHONE NUMBER": item.attributionInfo?.agentPhoneNumber || "N/A",
+    //     "BROKER EMAIL": "N/A"
+    //   });
+    // });
 
 
 
